2850dd2a469d2398c790185601a2699fb79fceb4
Thomaz <thomaz2pinheiro@gmail.com>
2023-05-02 23:34:06 -0300

    fim do visitor de tipo

--- src/MyParser.java
+++ src/MyParser.java
@@ -31,7 +31,7 @@ public class MyParser implements MyParserConstants {
             // programas com conflito: 3, 4, 5, 6, 7, 8
             // 3 e 7 falando que não foi declarada classe
             // 4, 5, 6, 7, 8 falando sobre identificador nao valido
-            Program raiz = new MyParser(new StringReader(readFile(filenames.get(2)))).Prog();
+            Program raiz = new MyParser(new StringReader(readFile(filenames.get(8)))).Prog();
 
             DepthFirstVisitor visitorTabela = new DepthFirstVisitor();
             raiz.accept(visitorTabela);
